b28b93ed162eb9048a603f9f5993813018578a18
ChocoliBao <837104975@qq.com>
2019-07-26 14:47:08 +0800

    Update verification code method.

--- src/main/java/cn/imustacm/user/IUsersService.java
+++ src/main/java/cn/imustacm/user/IUsersService.java
@@ -6,13 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-
 /**
- * 用户相关呢接口定义
+ *
+ * 用户相关的接口定义
  *
  * @author liandong
  * @since 2019-07-21
@@ -22,13 +18,16 @@ public interface IUsersService {
 
 
     /**
-     * 测试一下
-     *
+     * 测试
      * @return
      */
     @PostMapping("/register")
     Boolean register();
 
+    /**
+     * 获取验证码
+     * @return
+     */
     @GetMapping("/getVerification")
     JSONObject getVerification();
 
