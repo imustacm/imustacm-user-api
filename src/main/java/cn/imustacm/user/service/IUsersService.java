30135b82f3046d61f62d9670cb0f2aeaa3e8edf6
liandong <liandog@mmears.com>
2019-08-18 16:00:37 +0800

    梳理依赖并迁移feign中的接口至controller

--- src/main/java/cn/imustacm/user/service/IUsersService.java
+++ src/main/java/cn/imustacm/user/service/IUsersService.java
@@ -1,13 +1,9 @@
 package cn.imustacm.user.service;
 
 import cn.imustacm.common.domain.Resp;
-import cn.imustacm.user.dto.BindEmailDTO;
-import cn.imustacm.user.dto.LoginDTO;
-import cn.imustacm.user.dto.RegisterDTO;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * 用户服务接口定义
@@ -19,55 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 public interface IUsersService {
 
     /**
-     * 获取验证码
-     *
-     * @return
-     */
-    @GetMapping("/getVerification")
-    Resp getVerification();
-
-    /**
-     * 获取七牛云Token
-     *
-     * @return
-     */
-    @GetMapping("/getQiniuToken")
-    Resp getQiniuToken();
-
-    /**
-     * 用户注册
-     *
-     * @return
-     */
-    @PostMapping("/register")
-    Resp register(@RequestBody RegisterDTO registerDTO);
-
-    /**
-     * 用户登录
-     *
-     * @return
-     */
-    @PostMapping("/login")
-    Resp login(HttpServletResponse response, @RequestBody LoginDTO loginDTO);
-
-    /**
-     * 邮箱绑定
-     *
-     * @return
-     */
-    @PostMapping("/bindEmail")
-    Resp bindEmail(HttpServletRequest request, @RequestBody BindEmailDTO bindEmailDTO);
-
-    /**
-     * 邮箱验证
-     *
-     * @return
-     */
-    @GetMapping("/verifyEmail")
-    Resp verifyEmail(@RequestParam String id);
-
-    /**
-     * 分页查询用户信息
+     * 分页查询用户信息 (测试框架)
      *
      * @return
      */
